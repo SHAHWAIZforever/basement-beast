d4ee1cf285c686bb9fd57d11083985da24a69561
SHAHWAIZ <shahwaiztiger12@gmail.com>
2025-05-15 19:23:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -34,52 +34,95 @@ window.location.href = '<?= $nextPage ?>' + window.location.search;
 }
 
 
-// >>>>>>>>>>>>>>>>>>>>
+// // >>>>>>>>>>>>>>>>>>>>
+// class Slider {
+// constructor(sliderElement) {
+//   this.slider = sliderElement;
+//   this.slides = this.slider.querySelectorAll('.slide');
+//   this.dots = this.slider.querySelectorAll('.dot');
+//   this.prevButton = this.slider.querySelector('.prev');
+//   this.nextButton = this.slider.querySelector('.next');
+//   this.slideIndex = 0;
+
+//   this.showSlide(this.slideIndex);
+
+//   this.prevButton.addEventListener('click', () => this.changeSlide(-1));
+//   this.nextButton.addEventListener('click', () => this.changeSlide(1));
+
+//   this.dots.forEach((dot, i) => {
+//     dot.addEventListener('click', () => this.showSlide(i));
+//   });
+
+//   // Optional: auto-slide every 5 seconds
+//   setInterval(() => {
+//     this.changeSlide(1);
+//   }, 5000);
+// }
+
+// showSlide(index) {
+//   this.slides.forEach((slide, i) => {
+//     slide.classList.remove('active');
+//     if (this.dots[i]) this.dots[i].classList.remove('active');
+
+//     if (i === index) {
+//       slide.classList.add('active');
+//       if (this.dots[i]) this.dots[i].classList.add('active');
+//     }
+//   });
+
+//   this.slideIndex = index;
+// }
+
+// changeSlide(n) {
+//   this.slideIndex = (this.slideIndex + n + this.slides.length) % this.slides.length;
+//   this.showSlide(this.slideIndex);
+// }
+// }
+
+// // Initialize all sliders
+// document.querySelectorAll('.slider').forEach(sliderElement => {
+// new Slider(sliderElement);
+// });
 class Slider {
-constructor(sliderElement) {
-  this.slider = sliderElement;
-  this.slides = this.slider.querySelectorAll('.slide');
-  this.dots = this.slider.querySelectorAll('.dot');
-  this.prevButton = this.slider.querySelector('.prev');
-  this.nextButton = this.slider.querySelector('.next');
-  this.slideIndex = 0;
+  constructor(sliderElement) {
+    this.slider = sliderElement;
+    this.slides = this.slider.querySelectorAll('.slide');
+    this.dots = this.slider.querySelectorAll('.dot');
+    this.slideIndex = 0;
 
-  this.showSlide(this.slideIndex);
+    this.showSlide(this.slideIndex);
 
-  this.prevButton.addEventListener('click', () => this.changeSlide(-1));
-  this.nextButton.addEventListener('click', () => this.changeSlide(1));
+    this.dots.forEach((dot, i) => {
+      dot.addEventListener('click', () => this.showSlide(i));
+    });
 
-  this.dots.forEach((dot, i) => {
-    dot.addEventListener('click', () => this.showSlide(i));
-  });
+    // Auto-slide every 5 seconds
+    setInterval(() => {
+      this.changeSlide(1);
+    }, 5000);
+  }
 
-  // Optional: auto-slide every 5 seconds
-  setInterval(() => {
-    this.changeSlide(1);
-  }, 5000);
-}
+  showSlide(index) {
+    this.slides.forEach((slide, i) => {
+      slide.classList.remove('active');
+      if (this.dots[i]) this.dots[i].classList.remove('active');
 
-showSlide(index) {
-  this.slides.forEach((slide, i) => {
-    slide.classList.remove('active');
-    if (this.dots[i]) this.dots[i].classList.remove('active');
+      if (i === index) {
+        slide.classList.add('active');
+        if (this.dots[i]) this.dots[i].classList.add('active');
+      }
+    });
 
-    if (i === index) {
-      slide.classList.add('active');
-      if (this.dots[i]) this.dots[i].classList.add('active');
-    }
-  });
+    this.slideIndex = index;
+  }
 
-  this.slideIndex = index;
-}
-
-changeSlide(n) {
-  this.slideIndex = (this.slideIndex + n + this.slides.length) % this.slides.length;
-  this.showSlide(this.slideIndex);
-}
+  changeSlide(n) {
+    this.slideIndex = (this.slideIndex + n + this.slides.length) % this.slides.length;
+    this.showSlide(this.slideIndex);
+  }
 }
 
 // Initialize all sliders
 document.querySelectorAll('.slider').forEach(sliderElement => {
-new Slider(sliderElement);
+  new Slider(sliderElement);
 });
